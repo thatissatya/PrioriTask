168b19e928673040cf0536143a98675edf758990
Satya Prakash <satyaprakashksingh@gmail.com>
2023-03-02 20:35:36 +0530

    Login Page added

--- priori-task/src/App.jsx
+++ priori-task/src/App.jsx
@@ -1,17 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
+import { LoginPage } from './components/pages/loginPage/LoginPage'
 
 function App() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
     return (
         <div className="ui-app">
-            <div className='ui-app-header'>
-                <Header />
-            </div>
-            <div className='ui-app-body'>
-                <div className='ui-app-body-side-navigation-page'></div>
-                <div className='ui-app-body-content'></div>
-            </div>
+            {
+                !isLoggedIn && <LoginPage/>
+            }
         </div>
     )
 }
