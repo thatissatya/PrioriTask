d880ea5c9f6c110c36117889b1a11c34b21a0b3b
Deepak Aggarwal <deepak.aggarwal@Deepak-Aggarwal-MBP.local>
2023-03-04 23:01:48 +0530

    page filter added

--- src/components/pages/dashboardPage/DashboardPage.jsx
+++ src/components/pages/dashboardPage/DashboardPage.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import './DashboardPage.css'
 import { logout } from '../../../config/firebase'
 import { SidebarNav } from '../../SidebarNav/SidebarNav'
+import { PageFilter } from '../../PageFilter/PageFilter'
 
 const _DashboardPage = () => {
     return (
         <div className='ui-dashboard-page'>
             <SidebarNav />
-            <div className='ui-dashboard-page-logout-btn'>
-            <button onClick={() => logout()}  > Logout </button>
+            <div className='ui-dashboard-page__main'>
+                <div className='ui-dashboard-page-logout-btn'>
+                <button onClick={() => logout()}  > Logout </button>
+                </div>
+                <PageFilter/>
             </div>
         </div>
     )
