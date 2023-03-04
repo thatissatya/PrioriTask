7b6ce3d02a1ab484794ff3e52a13b0396293eea2
Satya Prakash <satyaprakashksingh@gmail.com>
2023-03-04 21:21:23 +0530

    sidebar navigation added

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import Header from './components/Header/Header'
 import { LoginPage } from './components/pages/loginPage/LoginPage'
 import { DashboardPage } from './components/pages/dashboardPage/DashboardPage'
 
-import { SidebarNav } from './components/SidebarNav/SidebarNav'
-
 const _App = (props)=> {
     return (
         <div className="ui-app">
--- src/components/pages/dashboardPage/DashboardPage.jsx
+++ src/components/pages/dashboardPage/DashboardPage.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './DashboardPage.css'
 import { logout } from '../../../config/firebase'
-const  _DashboardPage = () => {
-  return (
-    <div className='ui-dashboard-page'>
-        <button onClick={()=> logout() }  > Logout </button>
-    </div>
-  )
+import { SidebarNav } from '../../SidebarNav/SidebarNav'
+
+const _DashboardPage = () => {
+    return (
+        <div className='ui-dashboard-page'>
+            <SidebarNav />
+            <div className='ui-dashboard-page-logout-btn'>
+            <button onClick={() => logout()}  > Logout </button>
+            </div>
+        </div>
+    )
 }
 
-export const DashboardPage =  _DashboardPage
+export const DashboardPage = _DashboardPage
