2fab6b7bb89fe92d563a1f36e44cf8de3bbf70c4
Satya Prakash <satyaprakash@Satyas-MacBook-Air.local>
2023-07-01 13:09:25 +0530

    code refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/Header/Header'
 import { LoginPage } from './components/pages/loginPage/LoginPage'
 import { DashboardPage } from './components/pages/dashboardPage/DashboardPage'
 
